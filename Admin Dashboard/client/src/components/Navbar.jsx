3931f9a4fac3c80dc66857ae74ca9889ebac72e2
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-12-24 17:30:17 +0530

    Update Navbar.jsx

--- Admin Dashboard/client/src/components/Navbar.jsx
+++ Admin Dashboard/client/src/components/Navbar.jsx
@@ -8,8 +8,9 @@ import {
   ArrowDropDownOutlined,
 } from "@mui/icons-material";
 import FlexBetween from "components/FlexBetween";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setMode } from "state";
+import styled from "styled-components";
 import profileImage from "assets/Profile2.jpg";
 
 import {
@@ -21,14 +22,16 @@ import {
   InputBase,
   Toolbar,
   Menu,
-  MenuItem,
+  // MenuItem,
   useTheme,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
 
+  const auth = useSelector((state) => state.auth);
   const [anchorEl, setAnchorEl] = useState(null);
   const isOpen = Boolean(anchorEl);
   const handleClick = (event) => setAnchorEl(event.currentTarget);
@@ -43,89 +46,117 @@ const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
       }}
     >
       <Toolbar sx={{ justifyContent: "space-between" }}>
-        {/* LEFT SIDE */}
-        <FlexBetween>
-          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-            <MenuIcon />
-          </IconButton>
-          <FlexBetween
-            backgroundColor={theme.palette.background.alt}
-            borderRadius="9px"
-            gap="3rem"
-            p="0.1rem 1.5rem"
-          >
-            <InputBase placeholder="Search..." />
-            <IconButton>
-              <Search />
-            </IconButton>
-          </FlexBetween>
-        </FlexBetween>
+        {auth._id ? (
+          <>
+            {/* LEFT SIDE */}
+            <FlexBetween>
+              <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                <MenuIcon />
+              </IconButton>
+              <FlexBetween
+                backgroundColor={theme.palette.background.alt}
+                borderRadius="9px"
+                gap="3rem"
+                p="0.1rem 1.5rem"
+              >
+                <InputBase placeholder="Search..." />
+                <IconButton>
+                  <Search />
+                </IconButton>
+              </FlexBetween>
+            </FlexBetween>
 
-        {/* RIGHT SIDE */}
-        <FlexBetween gap="1.5rem">
-          <IconButton onClick={() => dispatch(setMode())}>
-            {theme.palette.mode === "dark" ? (
-              <DarkModeOutlined sx={{ fontSize: "25px" }} />
-            ) : (
-              <LightModeOutlined sx={{ fontSize: "25px" }} />
-            )}
-          </IconButton>
-          <IconButton>
-            <SettingsOutlined sx={{ fontSize: "25px" }} />
-          </IconButton>
+            {/* RIGHT SIDE */}
+            <FlexBetween gap="1.5rem">
+              <IconButton onClick={() => dispatch(setMode())}>
+                {theme.palette.mode === "dark" ? (
+                  <DarkModeOutlined sx={{ fontSize: "25px" }} />
+                ) : (
+                  <LightModeOutlined sx={{ fontSize: "25px" }} />
+                )}
+              </IconButton>
+              <IconButton>
+                <SettingsOutlined sx={{ fontSize: "25px" }} />
+              </IconButton>
 
-          <FlexBetween>
-            <Button
-              onClick={handleClick}
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                textTransform: "none",
-                gap: "1rem",
-              }}
-            >
-              <Box
-                component="img"
-                alt="profile"
-                src={profileImage}
-                height="32px"
-                width="32px"
-                borderRadius="50%"
-                sx={{ objectFit: "cover" }}
-              />
-              <Box textAlign="left">
-                <Typography
-                  fontWeight="bold"
-                  fontSize="0.85rem"
-                  sx={{ color: theme.palette.secondary[100] }}
+              <FlexBetween>
+                <Button
+                  onClick={handleClick}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    textTransform: "none",
+                    gap: "1rem",
+                  }}
                 >
-                  {user.name}
-                </Typography>
-                <Typography
-                  fontSize="0.75rem"
-                  sx={{ color: theme.palette.secondary[200] }}
+                  <Box
+                    component="img"
+                    alt="profile"
+                    src={profileImage}
+                    height="32px"
+                    width="32px"
+                    borderRadius="50%"
+                    sx={{ objectFit: "cover" }}
+                  />
+                  <Box textAlign="left">
+                    <Typography
+                      fontWeight="bold"
+                      fontSize="0.85rem"
+                      sx={{ color: theme.palette.secondary[100] }}
+                    >
+                      {user.name}
+                    </Typography>
+                    <Typography
+                      fontSize="0.75rem"
+                      sx={{ color: theme.palette.secondary[200] }}
+                    >
+                      {user.occupation}
+                    </Typography>
+                  </Box>
+                  <ArrowDropDownOutlined
+                    sx={{
+                      color: theme.palette.secondary[300],
+                      fontSize: "25px",
+                    }}
+                  />
+                </Button>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={isOpen}
+                  onClose={handleClose}
+                  anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
                 >
-                  {user.occupation}
-                </Typography>
-              </Box>
-              <ArrowDropDownOutlined
-                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
-              />
-            </Button>
-            <Menu
-              anchorEl={anchorEl}
-              open={isOpen}
-              onClose={handleClose}
-              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-            >
-              <MenuItem onClick={handleClose}>Log Out</MenuItem>
-            </Menu>
+                  <Logout onClick={handleClose}>Logout</Logout>
+                </Menu>
+              </FlexBetween>
+            </FlexBetween>
+          </>
+        ) : (
+          <FlexBetween gap="1.5rem">
+            <AuthLinks>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </AuthLinks>
           </FlexBetween>
-        </FlexBetween>
+        )}
       </Toolbar>
     </AppBar>
   );
 };
 
 export default Navbar;
+
+const AuthLinks = styled.div`
+  a {
+    color: white;
+    &:last-child {
+      margin-left: 2rem;
+    }
+  }
+`;
+
+const Logout = styled.div`
+  color: white;
+  cursor: pointer;
+`;
