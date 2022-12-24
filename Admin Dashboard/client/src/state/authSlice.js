8d46357cfb2590a7d2fc9bb8773490f5a4072478
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-12-24 17:30:26 +0530

    Create authSlice.js

---
+++ Admin Dashboard/client/src/state/authSlice.js
@@ -0,0 +1,76 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  name: "",
+  email: "",
+  _id: "",
+  registerStatus: "",
+  registerError: "",
+  loginStatus: "",
+  loginError: "",
+  userLoaded: false,
+};
+
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (user, { rejectWithValue }) => {
+    try {
+      const token = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/register`,
+        {
+          name: user.name,
+          email: user.email,
+          password: user.password,
+          city: user.city,
+          state: user.state,
+          country: user.country,
+          occupation: user.occupation,
+          phoneNumber: user.phoneNumber,
+          role: user.role,
+        }
+      );
+      localStorage.setItem("token", token.data);
+      return token.data;
+    } catch (err) {
+      console.log(err.response.data);
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(registerUser.pending, (state, action) => {
+      return { ...state, registerStatus: "pending" };
+    });
+    builder.addCase(registerUser.fulfilled, (state, action) => {
+      if (action.payload) {
+        const user = jwtDecode(action.payload);
+
+        return {
+          ...state,
+          token: action.payload,
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+          registerStatus: "success",
+        };
+      } else return state;
+    });
+    builder.addCase(registerUser.rejected, (state, action) => {
+      return {
+        ...state,
+        registerStatus: "rejected",
+        registerError: action.payload,
+      };
+    });
+  },
+});
+
+export default authSlice.reducer;
