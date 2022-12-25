d88c8b8c972d5fd87a1cdc8f698a8717e6af7e6b
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-12-25 17:02:20 +0530

    Update Login.jsx

--- Admin Dashboard/client/src/scenes/auth/Login.jsx
+++ Admin Dashboard/client/src/scenes/auth/Login.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { loginUser } from "state/authSlice";
+import { useSelector } from "react-redux";
+// import { loginUser } from "state/authSlice";
 import { StyledForm } from "./StyledForm";
-import { useNavigate } from "react-router-dom";
-import Navbar from "components/Navbar";
+import { Link, useNavigate } from "react-router-dom";
+// import Navbar from "components/Navbar";
 import axios from "axios";
 
 const Login = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const navigate = useNavigate();
   const auth = useSelector((state) => state.auth);
   // console.log("🚀 ~ file: register.jsx:8 ~ Register ~ auth", auth);
@@ -46,29 +46,34 @@ const Login = () => {
   };
 
   return (
-    <>
-      <Navbar />
+    <div style={{ margin: "auto" }}>
       <StyledForm onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <input
-          type="email"
-          placeholder="email"
-          required
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          required
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-        />
-        <button>
-          {auth.loginStatus === "pending" ? "Submitting..." : "Login"}
-        </button>
+        <div style={{ margin: "auto" }}>
+          <h2>Login</h2>
+          <input
+            type="email"
+            placeholder="email"
+            required
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            required
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+          <button>
+            {auth.loginStatus === "pending" ? "Submitting..." : "Login"}
+          </button>
 
-        {auth.loginStatus === "rejected" ? <p>{auth.loginError}</p> : null}
+          <p style={{ color: "white" }}>
+            Don't have an account? <Link to="/register">Register</Link>{" "}
+          </p>
+
+          {auth.loginStatus === "rejected" ? <p>{auth.loginError}</p> : null}
+        </div>
       </StyledForm>
-    </>
+    </div>
   );
 };
 
